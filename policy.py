4c91c891ffb84fd7bc26abc73c0627142065df62
codearslan <hasan.arslan@gmail.com>
2026-03-09 05:45:00 -0400

    orchid post-MD boost 1.4->1.5x

--- policy.py
+++ policy.py
@@ -71,7 +71,7 @@ def compute_order(day, product, sales_history, order_history):
     elif product == 'orchids' and VALENTINES - day == -1:
         est *= 1.2
     elif product == 'orchids' and MOTHERS_DAY - day == -1:
-        est *= 1.4
+        est *= 1.5
 
     if product == 'tulips' and VALENTINES - day == 0:
         est *= 3.8
