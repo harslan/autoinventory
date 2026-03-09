7328901549c44e8fec5603932a6115b06ebe93a7
codearslan <hasan.arslan@gmail.com>
2026-03-09 06:13:36 -0400

    orchid Valentine 2.4->2.6x

--- policy.py
+++ policy.py
@@ -66,7 +66,7 @@ def compute_order(day, product, sales_history, order_history):
             est *= 3.0
 
     if product in ('orchids', 'lilies') and VALENTINES - day == 0:
-        est *= 2.4 if product == 'orchids' else 1.9
+        est *= 2.6 if product == 'orchids' else 1.9
     elif product == 'orchids' and VALENTINES - day == 1:
         est *= 1.3
     elif product == 'orchids' and VALENTINES - day == -1:
