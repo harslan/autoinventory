c3e5114a827d26eeb4df3cf0600ec286a0689010
codearslan <hasan.arslan@gmail.com>
2026-03-09 07:03:26 -0400

    sunflower post-MD 1.5->1.6x in new context

--- policy.py
+++ policy.py
@@ -76,7 +76,7 @@ def compute_order(day, product, sales_history, order_history):
     elif product == 'lilies' and MOTHERS_DAY - day == -1:
         est *= 1.9
     elif product == 'sunflowers' and MOTHERS_DAY - day == -1:
-        est *= 1.5
+        est *= 1.6
 
     if product == 'tulips' and VALENTINES - day == 0:
         est *= 3.8
