02046889035193566eeca4f50cd39d784d638f87
codearslan <hasan.arslan@gmail.com>
2026-03-09 10:37:32 -0400

    rose pre-MD dt=6 1.5->1.7x

--- policy.py
+++ policy.py
@@ -93,7 +93,7 @@ def compute_order(day, product, sales_history, order_history):
     elif product == 'tulips' and MOTHERS_DAY - day == -1:
         est *= 1.3
     elif product == 'roses' and MOTHERS_DAY - day == 6:
-        est *= 1.5
+        est *= 1.7
     elif product == 'roses' and MOTHERS_DAY - day == 3:
         est *= 1.05
     elif product == 'roses' and MOTHERS_DAY - day == 2:
